2210486d89158f685dbbcf3a612544d7bcda239f
Álvaro <128129748+blokclass@users.noreply.github.com>
2023-11-20 10:38:25 +0100

    Started API

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const personas = [
+    {
+        Pablo: "Gilipollas",
+        Antonio: "Fantabuloso",
+    },
+];
+
+app.get("/api", function (req, res) {
+    res.json(personas);
+});
+
+app.listen(PORT);
+1;
